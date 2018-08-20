882cef8cdab1a40df62f0d94a07ee47212f0eddd
RuchiraRuwantha <rruwantha222@gmail.com>
2018-08-21 00:40:35 +0530

    class FlyNoWay

---
+++ FlyNoWay.java
@@ -0,0 +1,6 @@
+class FlyNoWay implements FlyBehavior {
+
+    public void fly() {
+        System.out.println("I can not fly.");
+    }
+}
--- QuackBehavior.java
+++ QuackBehavior.java
@@ -1,5 +1,5 @@
 interface QuackBehavior {
 
     public void quack();
-
+    
 }
